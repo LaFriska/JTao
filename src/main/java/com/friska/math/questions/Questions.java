ee26c1d79fe3b59ba5eac69c093dad4e808c50ba
LaFriska <harold.gao205@gmail.com>
2024-01-04 22:35:18 +0800

    Fixed matching issues between matrix and vector dimensions in matrix vector multiplication

--- src/main/java/com/friska/math/questions/Questions.java
+++ src/main/java/com/friska/math/questions/Questions.java
@@ -8,7 +8,7 @@ public class Questions {
 
     public static Question matrixVectorMultiplication(int rowLowerBound, int rowUpperBound, int colLowerBound, int colUpperBound, int lowestPossibleEntry, int highestPossibleEntry){
         Matrix mat = QuestionUtil.getRandomIntegerMatrix(QuestionUtil.getRandomMatrixDimension(rowLowerBound, rowUpperBound, colLowerBound, colUpperBound), lowestPossibleEntry, highestPossibleEntry);
-        Vector vec = QuestionUtil.getRandomIntegerVector(mat.getRowLength(), lowestPossibleEntry, highestPossibleEntry);
+        Vector vec = QuestionUtil.getRandomIntegerVector(mat.getColLength(), lowestPossibleEntry, highestPossibleEntry);
         Matrix res = Linalg.multiply(mat, vec);
         return new Question(mat.getTex() + " " + vec.getTex(), res.getTex());
     }
